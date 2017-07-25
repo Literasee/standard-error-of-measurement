580281b557ba839507342b4f108419d5f4e594f3
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-07-25 15:11:52 -0400

    Update feature-aside.js

--- components/feature-aside.js
+++ components/feature-aside.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
 
-export default props => {
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        right: 0,
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        fontSize: '2rem',
-        width: '30vw',
-        padding: '0 2rem'
-      }}
-    >
-      {props.children}
-    </div>
-  )
+class FeatureContent extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          right: 0,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          fontSize: '2rem',
+          width: '30vw',
+          padding: '0 2rem'
+        }}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
 }
+
+module.exports = FeatureAside
