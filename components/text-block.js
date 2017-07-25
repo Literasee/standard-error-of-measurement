6f95a47bfb2f4439ed3321b980f63809a11ce215
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-07-25 15:12:10 -0400

    Update text-block.js

--- components/text-block.js
+++ components/text-block.js
@@ -1,30 +1,69 @@
 import React, { Component } from 'react'
 
-export default () =>
-  <div style={{
-    width: '60vw',
-    padding: '2rem',
-    borderBottom: '1px solid darkred',
-    backgroundColor: '#EEE'
-  }}>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi rem dolorem eveniet? Cum facilis odit provident porro ducimus consequatur voluptate voluptatum, earum molestias, commodi repellendus quas sit? Temporibus, dolores, quisquam!
-    </p>
-    <p>Laboriosam, assumenda perspiciatis praesentium. Minus fugiat perspiciatis minima iusto eaque debitis ratione, ipsa. Adipisci mollitia deleniti ipsam quaerat consectetur, vel nihil doloremque laborum voluptatem! Odit sit enim, quasi repudiandae et.
-    </p>
-    <p>Earum fugit repellendus dolor, consequatur odit architecto asperiores alias distinctio deleniti ab! Accusamus distinctio error dolorem eius voluptas praesentium quibusdam corporis animi aliquid doloremque rem cupiditate reiciendis porro, non ducimus?
-    </p>
-    <p>Repellat nisi, consectetur itaque ipsam laborum, aspernatur officiis ab iusto odit cum perspiciatis necessitatibus nulla minus officia! Quas culpa ipsam consequuntur voluptas totam molestiae rerum explicabo natus illo, incidunt amet.
-    </p>
-    <p>Et nostrum placeat recusandae, non, repellendus numquam excepturi dolores ex molestias hic asperiores officiis quasi ea. Ut voluptates, commodi a laboriosam molestiae totam atque impedit tempora velit. Ullam, harum, amet.
-    </p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo tenetur sunt, saepe, nihil quis placeat, tempore repellat soluta voluptatem obcaecati optio molestiae consequuntur libero molestias necessitatibus. Architecto eum aperiam laboriosam.
-    </p>
-    <p>Totam quas beatae, labore facilis libero mollitia obcaecati dolores officiis debitis eum porro cumque fugit animi voluptatem ipsam, sed, non eligendi inventore, sapiente reprehenderit nobis qui id! Quos, voluptatum maxime?
-    </p>
-    <p>Perferendis nostrum dolor, ad officiis cumque, ea optio molestias eligendi quia sunt commodi animi veritatis, error tenetur enim labore suscipit, accusantium assumenda fugit sequi facilis atque! Obcaecati officiis deleniti, aspernatur!
-    </p>
-    <p>Aperiam quod fugiat, laborum facere at consequatur expedita sint esse deleniti debitis aut vel impedit cupiditate nihil praesentium dolore rerum, perspiciatis omnis aliquid, ullam quas voluptatum. Nam, natus laudantium error.
-    </p>
-    <p>Placeat illum fugiat voluptas saepe deserunt iste odit ipsam alias beatae unde rerum maxime dolore, magnam nostrum quia enim, ad non nobis ipsum! Veritatis magnam deleniti ab est accusantium porro.
-    </p>
-  </div>
+class FeatureContent extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          width: '60vw',
+          padding: '2rem',
+          borderBottom: '1px solid darkred',
+          backgroundColor: '#EEE'
+        }}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi rem dolorem eveniet?
+          Cum facilis odit provident porro ducimus consequatur voluptate voluptatum, earum
+          molestias, commodi repellendus quas sit? Temporibus, dolores, quisquam!
+        </p>
+        <p>
+          Laboriosam, assumenda perspiciatis praesentium. Minus fugiat perspiciatis minima iusto
+          eaque debitis ratione, ipsa. Adipisci mollitia deleniti ipsam quaerat consectetur, vel
+          nihil doloremque laborum voluptatem! Odit sit enim, quasi repudiandae et.
+        </p>
+        <p>
+          Earum fugit repellendus dolor, consequatur odit architecto asperiores alias distinctio
+          deleniti ab! Accusamus distinctio error dolorem eius voluptas praesentium quibusdam
+          corporis animi aliquid doloremque rem cupiditate reiciendis porro, non ducimus?
+        </p>
+        <p>
+          Repellat nisi, consectetur itaque ipsam laborum, aspernatur officiis ab iusto odit cum
+          perspiciatis necessitatibus nulla minus officia! Quas culpa ipsam consequuntur voluptas
+          totam molestiae rerum explicabo natus illo, incidunt amet.
+        </p>
+        <p>
+          Et nostrum placeat recusandae, non, repellendus numquam excepturi dolores ex molestias hic
+          asperiores officiis quasi ea. Ut voluptates, commodi a laboriosam molestiae totam atque
+          impedit tempora velit. Ullam, harum, amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo tenetur sunt, saepe, nihil
+          quis placeat, tempore repellat soluta voluptatem obcaecati optio molestiae consequuntur
+          libero molestias necessitatibus. Architecto eum aperiam laboriosam.
+        </p>
+        <p>
+          Totam quas beatae, labore facilis libero mollitia obcaecati dolores officiis debitis eum
+          porro cumque fugit animi voluptatem ipsam, sed, non eligendi inventore, sapiente
+          reprehenderit nobis qui id! Quos, voluptatum maxime?
+        </p>
+        <p>
+          Perferendis nostrum dolor, ad officiis cumque, ea optio molestias eligendi quia sunt
+          commodi animi veritatis, error tenetur enim labore suscipit, accusantium assumenda fugit
+          sequi facilis atque! Obcaecati officiis deleniti, aspernatur!
+        </p>
+        <p>
+          Aperiam quod fugiat, laborum facere at consequatur expedita sint esse deleniti debitis aut
+          vel impedit cupiditate nihil praesentium dolore rerum, perspiciatis omnis aliquid, ullam
+          quas voluptatum. Nam, natus laudantium error.
+        </p>
+        <p>
+          Placeat illum fugiat voluptas saepe deserunt iste odit ipsam alias beatae unde rerum
+          maxime dolore, magnam nostrum quia enim, ad non nobis ipsum! Veritatis magnam deleniti ab
+          est accusantium porro.
+        </p>
+      </div>
+    )
+  }
+}
+
+module.exports = FeatureAside
